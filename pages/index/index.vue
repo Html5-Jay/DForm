0b294306bb40cb1a8413b80a963797baa1b6841d
shiyj <28602893@qq.com>
2024-08-14 13:26:53 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,255 @@
+<template>
+	<view class="content">
+	  <d-form ref="daForm" v-model="form" :option="option">
+	    <template #cccs>
+	      <uv-input style="width: 200px" v-model="form['cccs']" placeholder="cccsss" />
+	    </template>
+	    <template #srkSuffix>
+	      <uv-code
+	          ref="uCode"
+	          seconds="20"
+	          changeText="X秒重新获取哈哈哈"
+	      ></uv-code>
+	      <uv-button
+	          text="tips"
+	          type="success"
+	          size="mini"
+	      ></uv-button>
+	    </template>
+	    <template #srkPrefix>
+	      <view>111123123</view>
+	    </template>
+	  </d-form>
+	  <view class="btn-list">
+	    <uv-button type="primary" @click="submit">提交表单</uv-button>
+	    <uv-button @click="reset">重置表单</uv-button>
+	  </view>
+	</view>
+</template>
+
+<script>
+	import DForm from '@/uni_modules/Jay-DForm/components/Jay-DForm/Jay-DForm.vue'
+	export default {
+		components: {
+			DForm
+		},
+		data() {
+			return {
+				form: {},
+				//表单配置项
+				option: {
+				  labelWidth: '60',
+				  borderBottom: true,
+				  border: 'none',
+				  columns: [
+				    {
+				      placeholder: "请选择年龄段",
+				      label: "年龄",
+				      type: "radio",
+				      prop: 'age',
+				      dictData: [
+				        {
+				          value: 2,
+				          label: "16~20岁",
+				        },
+				        {
+				          value: 3,
+				          label: "21~25岁",
+				        },
+				        {
+				          value: 4,
+				          label: "26~30岁",
+				        },
+				      ],
+				      rules: [
+				        {
+				          type: 'number',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      label: "插槽测试",
+				      prop: 'cccs',
+				      isSlot: true
+				    },
+				    {
+				      label: "输入框",
+				      prop: 'srk',
+				    },
+				    {
+				      label: "获客时间",
+				      type: "datetime",
+				      prop: 'time',
+				      mode: 'datetime', //	date为日期选择，time为时间选择，year-month为年月选择 ,datetime日期时间选择
+				      returnTimestamp: false, //value值是否返回时间搓格式
+				      rules: [
+				        {
+				          // type: 'number',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      placeholder: "请选择兴趣",
+				      label: "兴趣",
+				      type: "checkbox",
+				      prop: 'xq',
+				      dictData: [{
+				        value: 1,
+				        label: "篮球",
+				        disabled: true, //是否禁用
+				      },
+				        {
+				          value: 2,
+				          label: "足球",
+				        },
+				        {
+				          value: 3,
+				          label: "羽毛球",
+				        },
+				        {
+				          value: 4,
+				          label: "唱歌",
+				        },
+				        {
+				          value: 5,
+				          label: "街舞",
+				        },
+				      ],
+				      rules: [
+				        {
+				          type: 'array',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      placeholder: "选择日期",
+				      label: "日期",
+				      prop: 'date',
+				      type: "calendar",
+				      mode: 'range', //	mode为single只能选择单个日期 为multiple可以选择多个日期为range可以选择日期范围
+				      returnTimestamp: false, //value值是否返回时间搓格式
+				      rules: [
+				        {
+				          // type: 'number',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      placeholder: "(例如：前端、后端)",
+				      label: "经历",
+				      prop: 'textarea',
+				      type: "textarea",
+				      count: true,
+				      rules: [
+				        {
+				          // type: 'number',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      label: "数字键盘",
+				      prop: 'number',
+				      type: "number",
+				      dotDisabled: true,
+				      rules: [
+				        {
+				          type: 'string',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				
+				    {
+				      label: "性别",
+				      // pickerTitle: '性别1',
+				      type: "select",
+				      prop: 'sex',
+				      border: 'bottom',
+				      dictData: [{
+				        value: 1,
+				        label: "男",
+				      },
+				        {
+				          value: 2,
+				          label: "女",
+				        },
+				      ],
+				      rules: [
+				        {
+				          type: 'number',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				    {
+				      label: "孩子头像 ",
+				      type: "file",
+				      prop: 'file',
+				      maxCount: 1, //最大选择图片的数量
+				      multiple: false, //是否开启图片多选，部分安卓机型不支持  false	true
+				      accept: 'image', //接受的文件类型，file只支持H5（只有微信小程序才支持把accept配置为all、media）  可选 all | media | image | file | video
+				      capture: ['album', 'camera'], // 图片或视频拾取模式，当accept为image类型时设置capture可选额外camera可以直接调起摄像头 String | Array
+				      sizeType: ['original', 'compressed'], //original 原图，compressed 压缩图，默认二者都有，H5无效
+				      compressed: true, //当accept为video时生效，是否压缩视频，默认为true  Boolean	true	false
+				      camera: 'back', //当accept为video时生效，可选值为back或front  	String	back	-
+				      maxSize: Number.MAX_VALUE, // 选择单个文件的最大大小，单位B(byte)，默认不限制	String | Number
+				      previewImage: true, //是否在上传完成后展示预览图	Boolean	true	false
+				      disabled: false, //是否禁用
+				      rules: [
+				        {
+				          type: 'array',
+				          required: true,
+				          message: '此为必填字段',
+				          trigger: ['blur', 'change']
+				        }
+				      ],
+				    },
+				  ]
+				},
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+		  // 提交表单
+		  submit() {
+		    // this.$refs.daForm.submit()
+		    this.$refs.daForm.submit().then(res => {
+		      console.log('success')
+		    }).catch(error => {
+		      console.log(error)
+		    })
+		  },
+		  //重置表单
+		  reset() {
+		    this.$refs.daForm.reset()
+		  }
+		},
+	}
+</script>
+
+<style>
+	.content{
+	  padding: 32rpx;
+	}
+</style>
---
+++ uni_modules/Jay-DForm/components/Jay-DForm/Jay-DForm.vue
@@ -0,0 +1,461 @@
+<script>
+	export default {
+		props: {
+			option: {
+				type: Object,
+				default: () => {
+					return {
+						rules: {},
+						errorType: 'message',
+						borderBottom: true,
+						labelPosition: 'top',
+						labelWidth: '45',
+						labelAlign: 'left',
+						labelStyle: {},
+						columns: []
+					}
+				}
+			},
+      // #ifdef VUE3
+      modelValue: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      // #endif
+      // #ifdef VUE2
+      value: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      }
+      // #endif
+		},
+		data() {
+			return {
+				form: {},
+				currentItem: {}, // 当前操作的item
+        ops: {},
+				pickerConfig: {
+					title: '',
+					keyName: '',
+					columns: [],
+				}
+			}
+		},
+    watch: {
+      // #ifdef VUE3
+      modelValue: {
+        handler() {
+          this.form = {
+            ...this.form,
+            ...this.modelValue
+          }
+        },
+        deep: true
+      },
+      // #endif
+      // #ifdef VUE2
+      value: {
+        handler() {
+          this.form = {
+            ...this.form,
+            ...this.value
+          }
+        },
+        deep: true
+      }
+      // #endif
+
+    },
+		mounted() {
+			this.initForm()
+		},
+		computed: {
+		},
+		methods: {
+      deepClone(obj, cache = new WeakMap()) {
+        if (obj === null || typeof obj !== 'object') return obj;
+        if (cache.has(obj)) return cache.get(obj);
+        let clone;
+        if (obj instanceof Date) {
+          clone = new Date(obj.getTime());
+        } else if (obj instanceof RegExp) {
+          clone = new RegExp(obj);
+        } else if (obj instanceof Map) {
+          clone = new Map(Array.from(obj, ([key, value]) => [key, this.deepClone(value, cache)]));
+        } else if (obj instanceof Set) {
+          clone = new Set(Array.from(obj, value => this.deepClone(value, cache)));
+        } else if (Array.isArray(obj)) {
+          clone = obj.map(value => this.deepClone(value, cache));
+        } else if (Object.prototype.toString.call(obj) === '[object Object]') {
+          clone = Object.create(Object.getPrototypeOf(obj));
+          cache.set(obj, clone);
+          for (const [key, value] of Object.entries(obj)) {
+            clone[key] = this.deepClone(value, cache);
+          }
+        } else {
+          clone = Object.assign({}, obj);
+        }
+        cache.set(obj, clone);
+        return clone;
+      },
+
+			formatTime(timestamp, formatType) {
+				const date = new Date(timestamp);
+
+				if (isNaN(date)) {
+					return "Invalid Date";
+				}
+
+				let year = date.getFullYear();
+				let month = String(date.getMonth() + 1).padStart(2, "0");
+				let day = String(date.getDate()).padStart(2, "0");
+				let hours = String(date.getHours()).padStart(2, "0");
+				let minutes = String(date.getMinutes()).padStart(2, "0");
+				let seconds = String(date.getSeconds()).padStart(2, "0");
+
+				switch (formatType) {
+					case "date":
+						return `${year}-${month}-${day}`;
+					case "time":
+						return `${hours}:${minutes}`; //:${seconds}
+					case "year-month":
+						return `${year}-${month}`;
+					case "datetime":
+						return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+					default:
+						return "Invalid Format Type";
+				}
+			},
+			initForm() {
+				// console.log(this.option.columns, 'initForm')
+				const defaultForm = {}
+				const rules = {}
+        const _option = this.deepClone(this.option)
+        const defaultOps = {
+          errorType: 'message',
+          borderBottom: true,
+          labelPosition: 'top',
+          labelWidth: '45',
+          labelAlign: 'left',
+          labelStyle: {},
+        }
+        const slots = this.$slots
+        const slotsKeys = Object.keys(slots)
+				// 初始化字段
+        _option.columns.forEach(item => {
+					if (item.prop) {
+            if (['checkbox', 'file'].includes(item.type)) {
+              defaultForm[item.prop] = []
+            } else {
+              defaultForm[item.prop] = ''
+            }
+					}
+					if (item.rules) {
+						rules[item.prop] = item.rules
+            // 判断规则里是否有必填字段
+            if (item.rules) {
+              // 数组形式
+              if (Object.prototype.toString.call(item.rules) === "[object Array]") {
+                if (item.rules.some(rItem => rItem.required)) {
+                  item.required = true
+                }
+              }
+              if ( Object.prototype.toString.call(item.rules) === "[object Object]") {
+                if (item.rules.required) {
+                  item.required = true
+                }
+              }
+            }
+					}
+          /**
+           * 获取是否有插槽 目前文本框类型有前置 后置插槽
+           * type设置为input 或者没有设置默认是文本
+           */
+          if (item.type === 'input' || !item.type) {
+            slotsKeys.forEach(key => {
+              if (key === `${item.prop}Prefix`) {
+                item.isPrefix = true
+                item.prefixName = key
+              }
+              if (key === `${item.prop}Suffix`) {
+                item.isSuffix = true
+                item.suffixName = key
+              }
+            })
+          }
+				})
+				let form = Object.assign(defaultForm, this.value)
+				this.$set(this, 'form', form)
+        this.$set(this, 'ops', Object.assign(defaultOps, _option))
+				setTimeout(() => {
+					this.$refs.form.setRules(rules)
+				}, 20)
+			},
+			selectValue(value, list, keyName = 'label') {
+				if (value) {
+					return list.find(item => item.value === value)[keyName]
+				}
+				return ''
+			},
+			showSelect(item) {
+				this.currentItem = item
+				this.pickerConfig = {
+					...this.pickerConfig,
+					title: item.pickerTitle || '',
+					keyName: item.keyName || 'label',
+					dictData: [item.dictData],
+				}
+        this.$nextTick(() => {
+          const ref = this.$refs[item.type]
+          if (Object.prototype.toString.call(ref) === '[object Array]') {
+            this.$refs[item.type][0].open()
+          } else {
+            this.$refs[item.type].open()
+          }
+        })
+			},
+			selectConfirm($event) {
+				if (this.currentItem.type === 'select') {
+					// todo 还要判断多选情况
+					this.form[this.currentItem.prop] = $event.value[0].value
+				}
+        this.updateParentValue()
+			},
+			selectClose() {
+
+			},
+			radioChange(e, item) {
+        this.updateParentValue()
+			},
+      checkboxChange(e, item) {
+        console.log(e, item)
+        this.currentItem = item
+        this.form[item.prop] = e
+        this.updateParentValue()
+      },
+			datetimeConfirm($event) {
+        const item = this.currentItem
+        const formatValue = $event.mode === 'time' ? this.formatTime(this.getTimestampFromTime($event.value), $event
+            .mode) : this.formatTime($event.value, $event.mode)
+        this.form[item.prop] = item.returnTimestamp ? ($event.mode === 'time' ? this
+            .getTimestampFromTime($event.value) : $event.value) : formatValue; //是否返回时间搓
+        this.updateParentValue()
+			},
+      calendarConfirm($event) {
+        const item = this.currentItem
+        const dateString = $event.reduce((b, n) => (b + `,${n}`))
+        this.form[item.prop] = dateString
+        this.updateParentValue()
+      },
+      keyboardConfirm(e) {
+        console.log(e)
+      },
+      keyboardChange(value) {
+        const item = this.currentItem
+        this.form[item.prop] = `${this.form[item.prop]}${value}`
+        this.updateParentValue()
+      },
+      keyboardBackspace() {
+        // 删掉字符串最后一位
+        const item = this.currentItem
+        this.form[item.prop] = this.form[item.prop].slice(0, -1)
+        this.updateParentValue()
+      },
+			/**
+			 * validate 严格校验类型，需要指定type，比如type: 'number',
+			 */
+			submit() {
+				return this.$refs.form.validate()
+				// this.$refs.form.validate().then(res => {
+				//   console.log(res)
+				// }).catch(errors => {
+				//   console.log(errors)
+				// })
+			},
+			reset() {
+				this.$refs.form.resetFields()
+				this.$refs.form.clearValidate()
+			},
+      // 删除图片
+      deletePic($event, item) {
+        this.form[item.prop].splice($event.index, 1)
+        this.updateParentValue()
+      },
+      // 新增图片
+      afterRead($event, item) {
+        // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+        const value = item.multiple ? $event.file : [$event.file]
+        this.form[item.prop] = value
+        this.updateParentValue()
+      },
+      updateParentValue() {
+        // #ifdef VUE3
+        this.$emit('update:modelValue', this.form)
+        // #endif
+        // #ifdef VUE2
+        this.$emit("input", this.form)
+        // #endif
+      }
+		}
+	}
+</script>
+
+<template>
+	<view>
+		<uv-form
+      ref="form"
+      :model="form"
+    >
+			<template v-for="item in ops.columns">
+				<template v-if="item.type === 'select'">
+					<uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item" @click="showSelect(item)">
+						<uv-input
+              class="disabled"
+              disabled
+              :border="item.border || option.border"
+              :value="selectValue(form[item.prop], item.dictData, item.keyName)"
+							:placeholder="`${item.placeholder || '请选择' + item.label}`"
+              v-bind="item"
+            >
+						</uv-input>
+						<template v-slot:right>
+							<uv-icon name="arrow-down-fill"></uv-icon>
+						</template>
+					</uv-form-item>
+				</template>
+
+				<template v-else-if="item.type === 'radio'">
+					<uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item">
+						<uv-radio-group v-model="form[item.prop]" @change="radioChange($event, item)">
+							<uv-radio v-for="(radioItem, radioIndex) in item.dictData" :key="radioIndex" :label="radioItem.label"
+								:name="radioItem.value" :customStyle="{marginLeft: '8px', marginBottom: '8px'}" />
+						</uv-radio-group>
+					</uv-form-item>
+				</template>
+
+				<template v-else-if="item.type === 'datetime'">
+					<uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item" @click="showSelect(item)">
+            <uv-input
+              class="disabled"
+              :value="form[item.prop]" disabled
+              :placeholder="`${item.placeholder || '请选择' + item.label}`"
+              :border="item.border || option.border"
+              v-bind="item"
+            >
+            </uv-input>
+            <template v-slot:right>
+              <uv-icon name="arrow-down-fill"></uv-icon>
+            </template>
+					</uv-form-item>
+				</template>
+
+        <template v-else-if="item.type === 'calendar'">
+          <uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item" @click="showSelect(item)">
+            <uv-input
+              class="disabled"
+              disabled
+              :border="item.border || option.border"
+              :value="form[item.prop]"
+              :placeholder="`${item.placeholder || '请选择' + item.label}`"
+              v-bind="item"
+            >
+            </uv-input>
+            <template v-slot:right>
+              <uv-icon name="arrow-down-fill"></uv-icon>
+            </template>
+          </uv-form-item>
+        </template>
+
+        <template v-else-if="item.type === 'checkbox'">
+          <uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item">
+            <uv-checkbox-group :value="form[item.prop]" @change="checkboxChange($event, item)">
+              <uv-checkbox
+                :customStyle="{marginLeft: '8px', marginBottom: '8px'}"
+                v-for="(checkItem, checkIndex) in item.dictData"
+                :key="checkIndex"
+                :label="checkItem.label"
+                :name="checkItem.value"
+              ></uv-checkbox>
+            </uv-checkbox-group>
+          </uv-form-item>
+        </template>
+
+        <template v-else-if="item.type === 'textarea'">
+          <uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item">
+            <uv-textarea
+              v-model="form[item.prop]"
+              v-bind="item"
+              :border="item.border || option.border"
+              :placeholder="`${item.placeholder || '请选择' + item.label}`"
+            />
+          </uv-form-item>
+        </template>
+        <template v-else-if="item.type === 'number'">
+          <uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item" @click="showSelect(item)">
+            <uv-input
+              class="disabled"
+              disabled
+              v-model="form[item.prop]"
+              :border="item.border || option.border"
+              :placeholder="`${item.placeholder || '请输入' + item.label}`"
+              v-bind="item"
+            />
+          </uv-form-item>
+        </template>
+
+        <template v-else-if="item.type === 'file'">
+          <uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item">
+            <uv-upload
+              :fileList="form[item.prop]" :disabled="item.disabled" :accept="item.accept"
+              :capture="item.capture" :maxCount="item.maxCount" :sizeType="item.sizeType"
+              :compressed="item.compressed" :camera="item.camera" :multiple="item.multiple" :maxSize="item.maxSize"
+              :previewImage="item.previewImage" width="150rpx" height="150rpx" @afterRead="afterRead($event, item)"
+              @delete="deletePic($event, item)"
+            />
+          </uv-form-item>
+        </template>
+
+				<template v-else-if="item.type === 'input' || !item.type">
+					<uv-form-item :label="item.label" :prop="item.prop" :borderBottom="option.borderBottom" v-bind="item">
+            <template v-if="item.isSlot">
+              <slot :name="item.prop" />
+            </template>
+            <template v-else>
+              <uv-input
+                :border="item.border || option.border"
+                v-model="form[item.prop]"
+                @input="updateParentValue"
+                v-bind="item"
+                :placeholder="`${item.placeholder || '请输入' + item.label}`"
+              >
+                <template #prefix v-if="item.isPrefix">
+                  <slot :name="item.prefixName"></slot>
+                </template>
+                <template #suffix v-if="item.isSuffix">
+                  <slot :name="item.suffixName"></slot>
+                </template>
+              </uv-input>
+            </template>
+					</uv-form-item>
+				</template>
+			</template>
+
+		</uv-form>
+
+    <uv-keyboard ref="number" mode="number" :showCancel="false" :showConfirm="false" :dotDisabled="currentItem.dotDisabled" @confirm="keyboardConfirm" @change="keyboardChange" @backspace="keyboardBackspace" />
+    <uv-calendar ref="calendar" v-bind="currentItem" @confirm="calendarConfirm" />
+    <uv-datetime-picker v-if="currentItem.type === 'datetime'" ref="datetime" :value="form[currentItem.prop]" :mode="currentItem.mode" @confirm="datetimeConfirm" />
+		<uv-picker safeAreaInsetBottom ref="select" :title="pickerConfig.title" :keyName="pickerConfig.keyName"
+			:columns="pickerConfig.dictData" @confirm="selectConfirm($event)" @close="selectClose" />
+	</view>
+</template>
+
+<style scoped lang="scss">
+.disabled{
+  background-color: transparent!important;
+}
+</style>
